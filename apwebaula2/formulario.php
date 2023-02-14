02eb25a708b2acb77f768ef1def3eb45eab32728
EvaldoFernandes <116390121+EvaldoFernandes@users.noreply.github.com>
2023-02-14 11:34:22 -0300

    Add files via upload

---
+++ apwebaula2/formulario.php
@@ -0,0 +1,29 @@
+<!doctype html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Formulario</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+</head>
+
+<body>
+    <div class="alert alert-primary" role="alert" align="center">
+        <h1 style="color:black">Digite um valor</h1>
+    </div>
+    <br>
+    <br>
+    <form action="recebe.php" method="post" align="center">
+        <p>Valor: <input type="text" name="v" size="5">
+        </p>
+        <br>
+        <input type="submit" value="Enviar">
+    </form>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN"
+        crossorigin="anonymous"></script>
+</body>
+
+</html>
